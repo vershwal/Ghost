249463b0c33bf47e0ab498d1a0cd69b394e65ef3
Princi Vershwal <princi.vershwal@Princis-MacBook-Pro.local>
2023-06-15 13:13:35 +0530

    testing

--- ghost/collections/src/CollectionRepository.ts
+++ ghost/collections/src/CollectionRepository.ts
@@ -1,4 +1,5 @@
 import {Collection} from './Collection';
+//No change
 export interface CollectionRepository {
     save(collection: Collection): Promise<void>
     getById(id: string): Promise<Collection | null>
