74bad491ce2e68fe8b0b42944466ebcbaa75ec73
Princi Vershwal <princi.vershwal@Princis-MacBook-Pro.local>
2023-06-27 22:12:05 +0530

    Refactor code for removing bluebird from roles.js

--- ghost/core/core/server/api/endpoints/utils/serializers/output/roles.js
+++ ghost/core/core/server/api/endpoints/utils/serializers/output/roles.js
@@ -8,7 +8,7 @@ module.exports = {
         const roles = models.toJSON(frame.options);
 
         if (frame.options.permissions !== 'assign') {
-            frame.response = {
+            return frame.response = {
                 roles: roles
             };
         } else {
